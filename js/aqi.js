f4e62eaa02a373adf118759f7f6e7ea90f076849
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2025-09-10 20:20:45 +0530

    country code

--- js/aqi.js
+++ js/aqi.js
@@ -1,7 +1,6 @@
 fetch('./country.json')
     .then(response => response.json())
     .then(data => {
-        console.log(data[0].name)
         menuOptions(data)
     })
     .catch(error => console.error('Error fetching country data:', error));
@@ -35,17 +34,19 @@ function menuOptions(data){
     })
 }
 
-//testing fetch country code using restcountries api
-const countryName = 'United Kingdom'
-const country = `https://restcountries.com/v3.1/name/${countryName.toLowerCase()}`
-
-fetch(country)
-    .then(response => response.json())
-    .then(data => countryFunc(data))
-    .catch(error => console.error('Error fetching country data:', error));
-
-function countryFunc(data){
-    console.log(data[0].ccn3); //fetch country code
-}
-
 //API Key:72286423a155d841b461770267224a5d
+
+function displayResults(){
+    const country = document.getElementById('countryDropdown').value;
+    const city = document.getElementById('cityDropdown').value;
+    console.log(country, city);
+
+    const countryUrl = `https://restcountries.com/v3.1/name/${country.toLowerCase()}`
+    fetch(countryUrl)
+        .then(response => response.json())
+        .then(data => {
+            const countryCode = data[0].ccn3; //fetch country code
+            console.log(countryCode);
+        })
+        .catch(error => console.error('Error fetching country code data:', error));
+}
