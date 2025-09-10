dfdc101a8f757f3d3f1e62530a33d6adb2ff08aa
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2025-09-10 20:03:27 +0530

    country code

--- js/aqi.js
+++ js/aqi.js
@@ -35,17 +35,17 @@ function menuOptions(data){
     })
 }
 
-    // const countryName = 'United Kingdom'
-// const country = `https://restcountries.com/v3.1/name/${countryName.toLowerCase()}`
+//testing fetch country code using restcountries api
+const countryName = 'United Kingdom'
+const country = `https://restcountries.com/v3.1/name/${countryName.toLowerCase()}`
 
-// fetch(country)
-//     .then(response => response.json())
-//     .then(data => countryFunc(data))
-//     .catch(error => console.error('Error fetching country data:', error));
+fetch(country)
+    .then(response => response.json())
+    .then(data => countryFunc(data))
+    .catch(error => console.error('Error fetching country data:', error));
 
-
-// function countryFunc(data){
-//     console.log(data[0].ccn3); //fetch country code
-// }
+function countryFunc(data){
+    console.log(data[0].ccn3); //fetch country code
+}
 
 //API Key:72286423a155d841b461770267224a5d
